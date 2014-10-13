7cf74b1c7d55c5ac05cd61fcf66be3462d5d097c
ger0nim0 <len2koo@gmail.com>
2014-10-13 13:22:08 +0300

    Fixed a bug with opening file

--- GraphicalEditor/GraphicalEditor.cpp
+++ GraphicalEditor/GraphicalEditor.cpp
@@ -261,14 +261,12 @@ VOID SaveMetafile(HWND hWnd)
 
 VOID OpenImage(HWND hWnd, LPCWSTR fileName)
 {
-	HDC windowDC = GetDC(hWnd);
 	HENHMETAFILE hemf = GetEnhMetaFile(fileName);  
 	GetClientRect(hWnd, &rect); 
-	ClearWindow(windowDC);
+	ClearWindow(memoryDC);
 	RefreshMetafileDC(hWnd);
-	PlayEnhMetaFile(windowDC, hemf, &rect);
+	PlayEnhMetaFile(memoryDC, hemf, &rect);
 	PlayEnhMetaFile(metafileDC, hemf, &rect);
-	ReleaseDC(hWnd, windowDC); 
 	DeleteEnhMetaFile(hemf);
 }
 
